65579c32d538f2feb1d8115a4705fa15fe113e9a
Paul Gherghel <pagherghel@yahoo.com>
2024-05-24 22:26:42 +0300

    added insta posts

--- src/components/Test3/instafeed.jsx
+++ src/components/Test3/instafeed.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React from 'react';
 import { InstagramEmbed } from 'react-social-media-embed';
-
 
 
 const Instafeed = () => {
   return (
-    <div>
-      <h1 className='text-4xl text-center'>Instafeedtest</h1>
+    <div className="">
+      <h1 className="text-4xl text-center">Paul Prints Plastic</h1>
       <br />
-      <div className="flex flex-col justify-center items-center">
-        <div className="max-w-full w-[500px] sm:w-[400px]">
-          <InstagramEmbed url="https://www.instagram.com/p/C6D3hWAtMmp/" width={500} captioned />
+      <div className="flex flex-wrap justify-center">
+        <div className="w-full md:w-1/2 lg:w-1/3 lg:p-4 sm:p-0">
+          <div className="max-w-full">
+            <InstagramEmbed url="https://www.instagram.com/p/C6wiCdtrc57"  />
+          </div>
         </div>
-        <br />
-        <br />
-        <div className="max-w-full w-[500px] sm:w-[400px]">
-          <InstagramEmbed url="https://www.instagram.com/reel/CqVNoEAvynp" width={500} />
+        <div className="w-full md:w-1/2 lg:w-1/3 lg:p-4 sm:p-0">
+          <div className="max-w-full">
+            <InstagramEmbed url="https://www.instagram.com/p/C6wXePKsZYL"  />
+          </div>
         </div>
-        {/* apparently you need to remove the '/' at the end of the url for it to work */}
+        <div className="w-full md:w-1/2 lg:w-1/3 lg:p-4 sm:p-0">
+          <div className="max-w-full">
+            <InstagramEmbed url="https://www.instagram.com/p/C6wcIRwLhNt"  />
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/3 lg:p-4 sm:p-0">
+          <div className="max-w-full">
+            <InstagramEmbed url="https://www.instagram.com/p/C6wcdN9r7gr"  />
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/3 lg:p-4 sm:p-0">
+          <div className="max-w-full">
+            <InstagramEmbed url="https://www.instagram.com/p/C6wgxPYLcAw"  />
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/3 lg:p-4 sm:p-0">
+          <div className="max-w-full">
+            <InstagramEmbed url="https://www.instagram.com/p/C6whgBsLiOT"  />
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Instafeed
+export default Instafeed;
